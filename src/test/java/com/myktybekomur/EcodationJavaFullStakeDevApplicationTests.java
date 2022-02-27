a495929c8b4395c8a684c503f1ac001f15261303
MyktybekOmur <mykty.omurbekov@gmail.com>
2022-02-27 10:39:56 +0300

    Initial commit

---
+++ src/test/java/com/myktybekomur/EcodationJavaFullStakeDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myktybekomur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcodationJavaFullStakeDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
